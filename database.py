7a788e7966418fb051b0d8c3fbdc5323ed972782
MAJDOUB <abdelkarim.majdoub@capgemini.com>
2023-08-01 13:45:14 +0200

    Modify the data already inserted using QDialog.

---
+++ database.py
@@ -0,0 +1,42 @@
+from configparser import ConfigParser
+import os 
+import pymysql.connections as MySQLdb
+
+class Database:
+    def __init__(self):
+        # Fetch configuration Data
+        # Read config.ini file
+        config_object = ConfigParser()
+        self.path_to_config = os.path.dirname(os.path.dirname(__file__)) + "\system_files\config.ini"
+        config_object.read(self.path_to_config)
+
+        # Get the password
+        databaseInfo = config_object["DATABASECONFIG"]
+
+        self.DB_SERVER = databaseInfo["DB_SERVER"]
+        self.DB_USERNAME = databaseInfo["DB_USERNAME"]
+        self.DB_PASSWORD = databaseInfo["DB_PASSWORD"]
+        self.DB_NAME = databaseInfo["DB_NAME"]
+
+        self.conn = MySQLdb.Connection(host= self.DB_SERVER,
+                                     user = self.DB_USERNAME,
+                                     password= self.DB_PASSWORD, 
+                                     database= self.DB_NAME
+                                    )
+    
+    def connectToDB(self):
+        """
+        """
+        return self.conn
+        
+    def executeQuery(self, query, fetch ='one'):
+        """
+        """
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        if fetch =='one':
+            data = cursor.fetchone()
+        else:
+            data = cursor.fetchall()
+        return data
+
